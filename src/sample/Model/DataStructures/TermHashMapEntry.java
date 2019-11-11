223ded7d62812230e9a2d71fe01b312b70ea6b9b
yishaia zabary <yz@sky-pick.com>
2019-11-11 11:11:30 +0200

    Data structures package added, parser added (numbers only). read file not multi threaded done.

---
+++ src/sample/Model/DataStructures/TermHashMapEntry.java
@@ -0,0 +1,47 @@
+package sample.Model.DataStructures;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class TermHashMapDataStructure {
+    public class TermHashMapEntry {
+        public String value;
+        public int TF=0;
+
+
+        public ArrayList<Integer> termLocations = new ArrayList<>();
+
+        public TermHashMapEntry(String value) {
+            this.value = value;
+        }
+
+        public void addLocation(int location){
+            TF++;
+            termLocations.add(location);
+        }
+    }
+
+    public String value;
+    public int TF=0;
+    public HashMap<String, TermHashMapEntry> termsEntries = new HashMap<>();
+
+    public TermHashMapDataStructure() {
+
+    }
+    public void insert(String s, int iDFLocation){
+        String entryValue = s;
+        String key = s.toLowerCase();
+        if (termsEntries.containsKey(key)){
+            // אם אני בפנים, תעדכן לפי הצורך
+            TermHashMapEntry entry = termsEntries.get(key);
+            entry.update();
+        }else{
+
+        }
+    }
+
+    public void SaveToQueue(){
+        return;
+    }
+}
---
+++ src/sample/Model/Parser/IParser.java
@@ -0,0 +1,9 @@
+package sample.Model.Parser;
+
+import java.io.BufferedReader;
+
+public interface IParser {
+
+    public void Parse(String fullDoc) throws Exception;
+
+}
