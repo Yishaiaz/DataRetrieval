2924b6eb61b58d61e6131c27be253a1fdfb96a89
sahar ben baruch <saharbenbaruch@gmail.com>
2019-11-26 20:39:09 +0200

    In GUI- reset works, two paths , stemming checkBox.

--- src/sample/Controller/Controller.java
+++ src/sample/Controller/Controller.java
@@ -9,7 +9,8 @@ import java.util.Observer;
 public class Controller extends Observable implements Observer {
     private MyModel model;
     private MyView view;
-    public String path;
+    public String corpusPath;
+    public String postingFilesPath;
 
     public Controller(MyModel model, MyView view){
         this.model=model;
@@ -18,12 +19,25 @@ public class Controller extends Observable implements Observer {
     @Override
     public void update(Observable o, Object arg) {
         switch ((String)arg){
-            case "path":
-                model.testInitFileOfTest(path);
+            case "corpusPath":
+                model.testInitFileOfTest(corpusPath);
+                break;
+            case "postingFilesPath":
+                model.setPostingFilesPath(postingFilesPath);
+                break;
+            case "parseWithStemming":
+                model.startParse (true);
+                break;
+            case "parseWithoutStemming":
+                model.startParse (false);
+                break;
+
         }
     }
 
-    public void setPath(String path) {
-        this.path=path;
+    public void setCorpusPath(String corpusPath) {
+        this.corpusPath = corpusPath;
     }
+
+    public void setPostingFilesPath(String postingFilesPath) {this.postingFilesPath=postingFilesPath;}
 }
