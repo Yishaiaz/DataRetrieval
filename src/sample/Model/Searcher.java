8f4a29914cc2f53902cf37f022a8ace13a3c37e2
sahar ben baruch <saharbenbaruch@gmail.com>
2019-12-28 09:00:32 +0200

    fix looping over queries

--- src/sample/Model/Searcher.java
+++ src/sample/Model/Searcher.java
@@ -4,6 +4,14 @@ public class Searcher {
 
 
     public void search(Document query) {
+        RankedDocumentsStructure rankedDocumentsStructure=new RankedDocumentsStructure(query.getDocNo());
+
+    }
+
+    /**
+     * write ranked results to file.
+     */
+    public void writeResultsToFile(){
 
     }
 }
